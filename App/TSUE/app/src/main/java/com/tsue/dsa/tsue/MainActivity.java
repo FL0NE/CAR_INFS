16b5be25e927448f1cd40486cce993354487a25f
Fl0ne <FlippFloppMC@web.de>
2017-07-05 15:01:46 +0200

    rmv start_acitivty

--- App/TSUE/app/src/main/java/com/tsue/dsa/tsue/MainActivity.java
+++ App/TSUE/app/src/main/java/com/tsue/dsa/tsue/MainActivity.java
@@ -5,9 +5,19 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    //VARIABLEN
+
+    TextView hohe = (TextView) findViewById(R.id.hohe_wert);
+    TextView entfernung = (TextView) findViewById(R.id.entf_wert);
+    TextView gang = (TextView) findViewById(R.id.gang_wert);
+
+
+
+    //ENDE VARIABLEN
 
 
     @Override
@@ -15,7 +25,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // TEST
+        // Settings_Buton
 
         Button btn_einstellungen = (Button) findViewById(R.id.btn_enstllngn);
         btn_einstellungen.setOnClickListener(new View.OnClickListener() {
