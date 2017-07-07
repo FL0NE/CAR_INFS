27a7d083e041297d8aea7b6b4d66d231c1b91114
MaxiMinus57 <maxjo.richter@gmail.com>
2017-07-07 13:47:45 +0200

    xx

--- App/TSUE/app/src/main/java/com/tsue/dsa/tsue/SpeedActivity.java
+++ App/TSUE/app/src/main/java/com/tsue/dsa/tsue/SpeedActivity.java
@@ -1,7 +1,10 @@
 package com.tsue.dsa.tsue;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.github.anastr.speedviewlib.PointerSpeedometer;
 
 public class SpeedActivity extends AppCompatActivity {
 
