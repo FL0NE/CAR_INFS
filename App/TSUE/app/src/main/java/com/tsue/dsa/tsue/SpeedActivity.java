8518a20aa8c145ecfa0c516865ba7adc5094b7f4
For3stFrog <luis6291bt@gmail.com>
2017-07-07 12:41:08 +0200

    speedometer

--- App/TSUE/app/src/main/java/com/tsue/dsa/tsue/SpeedActivity.java
+++ App/TSUE/app/src/main/java/com/tsue/dsa/tsue/SpeedActivity.java
@@ -3,11 +3,23 @@ package com.tsue.dsa.tsue;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.github.anastr.speedviewlib.base.Gauge;
+
 public class SpeedActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_speed);
+
+
+        Gauge speedometer = (Gauge) findViewById(R.id.pointerSpeedometer);
+        speedometer.speedTo(50);
+        speedometer.setMaxSpeed(200);
+
+
+
+
+
     }
 }
